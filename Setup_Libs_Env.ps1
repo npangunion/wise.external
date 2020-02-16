7828e754112f369871987a51dfc5c511c8f7760c
npangunion <npangunion@gmail.com>
2020-02-16 12:14:47 +0900

    - 환경 변수 설정 스크립트 추가.

---
+++ Setup_Libs_Env.ps1
@@ -0,0 +1,19 @@
+﻿# change path appropriate for each user
+
+$WISE_EXT_HOME = "E:\wisemountain\wise.external\libs\"
+
+# Set boost home
+[Environment]::SetEnvironmentVariable("BOOST_HOME", $WISE_EXT_HOME + "boost_1_72_0", "User")
+
+# Set catch home 
+[Environment]::SetEnvironmentVariable("CATCH_HOME", $WISE_EXT_HOME + "Catch2-2.11.1", "User")
+
+# Set GLFW home
+[Environment]::SetEnvironmentVariable("GLFW_HOME", $WISE_EXT_HOME + "glfw-3.3.2", "User")
+
+
+# Set LIBSODIUM home
+[Environment]::SetEnvironmentVariable("LIBSODIUM_HOME", $WISE_EXT_HOME + "libsodium-1.0.18-RELEASE", "User")
+
+# Set SPDLOG home
+[Environment]::SetEnvironmentVariable("SPDLOG_HOME", $WISE_EXT_HOME + "spdlog-1.5.0", "User")
